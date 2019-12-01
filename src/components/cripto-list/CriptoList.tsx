94b9565841d5986375c1252409d0dbf4c0121d5b
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-12-01 20:33:49 +0100

    Print all currency data on ant design card component

--- src/components/cripto-list/CriptoList.tsx
+++ src/components/cripto-list/CriptoList.tsx
@@ -1,19 +1,28 @@
 import React from "react";
 import PropTypes from "prop-types";
 import CriptocurrencyData from "../../CriptocurrencyData";
+import { Card } from "antd";
 
 interface ListProps {
   data: CriptocurrencyData[];
+  loading: boolean;
 }
 
 const CriptoList: React.FC<ListProps> = props => {
-  const { data } = props;
+  const { data, loading } = props;
   return (
     <React.Fragment>
       {data.map((cripto: CriptocurrencyData) => {
         return (
           <li key={cripto.id} className="cripto-list__cripto">
-            <h3 className="cripto__name">{cripto.name}</h3>
+            <Card style={{ width: 300, marginTop: 16 }} loading={loading}>
+              <h3 className="cripto__name">{cripto.name}</h3>
+              <p className="cripto__dolar-price">{`Price USD: ${cripto.price_usd}`}</p>
+              <p className="cripto__symbol">{cripto.symbol}</p>
+              <p className="cripto__change-1h">{`Change(1h): ${cripto.percent_change_1h}`}</p>
+              <p className="cripto__change-24h">{`Change(24h): ${cripto.percent_change_24h}`}</p>
+              <p className="cripto__btc-price">{`${cripto.price_btc} btc`}</p>
+            </Card>
           </li>
         );
       })}
