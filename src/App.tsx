d1403da2e20217c2bf7cb1b6eaa0302a08d15b56
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-12-01 19:21:27 +0100

    Fetch data from json cyrrency

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [data, setData] = useState([]);
+  const criptoApi = "https://api.coinmarketcap.com/v1/ticker/?convert=USD&limit=100";
+  const fetchCriptoData = () => {
+    fetch(criptoApi)
+      .then(response => response.json())
+      .then(criptoData => {
+        setData(criptoData);
+        console.log(criptoData);
+      });
+  };
+
+  useEffect(() => {
+    fetchCriptoData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hola</h1>
+      <header className="App__header">
+        <h1 className="header__title">Criptocurrencies prices</h1>
       </header>
+      <main className="app__main">
+      </main>
     </div>
   );
 };
